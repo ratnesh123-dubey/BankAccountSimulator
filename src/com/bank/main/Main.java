51904b074675eada726c5d3eac97b3eba8274b68
Ratnesh <dubeyratnesh60@gmail.com>
2026-07-07 12:26:59 +0530

    Rename package from com.ratnesh.bank to com.bank

--- src/com/bank/main/Main.java
+++ src/com/bank/main/Main.java
@@ -1,4 +1,4 @@
-package com.ratnesh.bank.main;
+package com.bank.main;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
